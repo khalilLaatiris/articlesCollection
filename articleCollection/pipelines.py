ce6affefb857d6b7f2614dd84a28ce12bd1706dc
khalilLaatiris <khalil.laatiris@uit.ac.ma>
2021-08-17 12:31:26 +0100

    The Guardians Article Collector
    the Scrapy project used to collect data form the Guardians web site

---
+++ articleCollection/pipelines.py
@@ -0,0 +1,35 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo
+from scrapy.utils.project import get_project_settings
+settings = get_project_settings() 
+class ArticlecollectionPipeline:
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+        return item
+
+
+
+
+
---
+++ articleCollection/spiders/Article.py
@@ -0,0 +1,28 @@
+from scrapy import Spider
+from articleCollection.items import ArticlecollectionItem
+
+class ActicleSpider(Spider):
+    name = "Article"
+    allowed_domains = ["theguardian.com"]
+    start_urls = [
+            'http://www.theguardian.com/international',
+            #'http://www.theguardian.com',
+        ]
+
+    def parse(self, response):
+    	# get every article link from the home page of the web
+    	articles=response.css('li.fc-slice__item div.fc-item__container')
+    	for article in articles:
+    		yield response.follow(article.css('a.u-faux-block-link__overlay::attr(href)').get(),callback=self.parse_article)
+    def parse_article(self,response):
+    	#get information from every article in the web site the guardiens 
+    	item=ArticlecollectionItem()
+
+    	item['headline']=response.css('article div > h1::text').get()
+    	item['link']=response.request.url
+    	item['author']=response.css('article address a::text').get()
+    	item['text']='\n'.join(response.css('article main div p::text').getall())
+    	item['image']=response.css('article picture img::attr(src)').get()
+    	item['subject']=response.css('article div a > span::text').get()
+    	item['date_time']=response.css('div.dcr-s64set label.dcr-hn0k3p::text').get()
+    	yield item
