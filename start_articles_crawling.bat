65e75207fa10a10b3d293091049a4c7967c88d53
khalilLaatiris <khalil.laatiris@uit.ac.ma>
2021-08-29 17:21:48 +0100

    Update start_articles_crawling.bat

--- start_articles_crawling.bat
+++ start_articles_crawling.bat
@@ -1,5 +1,5 @@
 @echo crawling articles from the guardiens web site will start now
-@@ type your home directory for the virtual envirenment
+:: type your home directory for the virtual envirenment
 
 cd C:\Users\khali\news  
 
